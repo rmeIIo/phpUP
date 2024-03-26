73f89834215260e9390615dedf70b7ef16f4c484
Roger Correa <j.rogercorreamello@gmail.com>
2024-03-26 13:12:01 -0300

    2603

---
+++ 1903/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Exercícios - Aula 03</title>
+</head>
+<body>
+    <main class="container">
+        <div class="title">
+            <h1>Exercícios - Aula 04</h1>
+            <h2>Desenvolvimento de Sistemas</h2>
+        </div>
+
+        <section class="exercise01">
+            <div class="titleExercise">
+                <h3>Exercício 01</h3>
+            </div>
+
+            <?php
+            ?>
+        </section>
+
+        <section class="exercise02">
+            <div class="titleExercise">
+                <h3>Exercício 02</h3>
+            </div>
+
+            <?php 
+            ?>
+        </section>
+
+        <section class="exercise01">
+            <div class="titleExercise">
+                <h3>Exercício 03</h3>
+            </div>
+
+            <?php 
+            ?>
+        </section>
+
+        <section class="exercise02">
+            <div class="titleExercise">
+                <h3>Exercício 04</h3>
+            </div>
+
+            <?php 
+            ?>
+        </section>
+
+        <section class="exercise01">
+            <div class="titleExercise">
+                <h3>Exercício 05</h3>
+            </div>
+
+            <?php 
+            ?>
+        </section>
+    </main>
+</body>
+</html>
